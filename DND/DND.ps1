0010722d6ffd957103312d0910c127943949a4e2
Anonymous <106942782+MajorMarmalade@users.noreply.github.com>
2024-11-10 13:54:37 -0500

    Update DND.ps1

--- DND/DND.ps1
+++ DND/DND.ps1
@@ -1,3 +1,18 @@
+# Import the System.Windows.Forms assembly
+Add-Type -AssemblyName System.Windows.Forms
+
+# Store the original position of the cursor
+$originalPOS = [System.Windows.Forms.Cursor]::Position
+
+# Loop until the cursor position changes
+while (1) {
+    # Check if the cursor position has changed
+    if ([Windows.Forms.Cursor]::Position -ne $originalPOS) {
+        # If the cursor has moved, exit the loop
+        break
+    }
+}
+
 function Show-ChoiceDialog {
     param (
         [string]$Title,
